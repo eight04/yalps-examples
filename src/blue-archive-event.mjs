5d70e465308f8ee8f5331fa0c99e3df21e0d7843
eight04 <eight04@gmail.com>
2023-09-02 16:11:47 +0800

    Update

--- src/blue-archive-event.mjs
+++ src/blue-archive-event.mjs
@@ -1,82 +1,123 @@
 import {solve, greaterEq, lessEq} from "yalps";
 
 const dailyGain = {
-  p1: 0,
-  p2: 0,
-  p3: 0,
-  p4: 40+40+40+50+50+40+50+50
+  // p4: 40+40+40+50+50+40+50+50
+};
+
+// FIXME: we sum dailyConstraint into dailyConstraint * daysLeft
+// which is wrong, the constraint has to be met every day
+const dailyConstraint = {
+  // p1: 300,
+  // p2: 260,
+  // p3: 210,
 };
 
 const dailyMultiplier = 1;
-const daysLeft = 0;
+const endDate = new Date("2023-09-05T09:59");
+const daysLeft = Math.floor((endDate - Date.now()) / (1000 * 60 * 60 * 24));
 
-const constraints = {
-  p1: greaterEq(
+const current = {
+  p1: 11490,
+  p2: 7439,
+  p3: 8565,
+}
+
+const shop = {
+  p1:
+    25*50
+    + 100*3+40*10+20*25+5*50
+    + 500+100*10+25*40+10*200
+    + 50*7+35*12+25*24+10*60
+    + 50*7+35*12+25*24+10*60
+    + 2000,
+  p2: (
     50*25
-    + 300+1500+2800+3000
-    + 75*7+50*12+35*24+15*60
-    + 2000
-    - 1688
-    - dailyGain.p1 * dailyMultiplier * daysLeft
-),
-  p2: greaterEq(
-    50*20
-    + 45*20+32*40+24*100+8*200
-    + 2000
-    - 1202
-    - dailyGain.p2 * dailyMultiplier * daysLeft
+    + 100*3+40*10+20*2+5*50
+    + 20*20+15*40+12*100+4*200
+    + 20*20+15*40+12*100+4*200
   ),
-  p3: greaterEq(
-    1000
-    + 100*5+40*25
-    + 150*3+60*10+30*25+15*50
-    + 150*3+60*10+30*25+15*50
-    + 500
-    - 1030
-    - dailyGain.p3 * dailyMultiplier * daysLeft
+  p3: (
+    50*25
+    + 100*3+40*10+20*25+5*50
+    + 100*5+40*25+20*100+5*500
   ),
-  p4: greaterEq(
-    15000
-    - 1514
-    - dailyGain.p4 * dailyMultiplier * daysLeft
-  )
+}
+
+const stages = {
+  controlAlpha: {
+    cost: 20,
+    p1: 40+48,
+  },
+  recordDelta: {
+    cost: 20,
+    p1: 5+6,
+    p2: 5+6,
+    p3: 30+36
+  },
+  recordBeta: {
+    cost: 20,
+    p1: 5+6,
+    p2: 30+36,
+    p3: 5+6
+  },
 };
 
-const variables = {
-  s9: {
-    cost: 20,
-    p1: 34+41,
-    p4: 15+17
-  },
-  s10: {
-    cost: 20,
-    p2: 29+29,
-    p4: 15+17,
-  },
-  s11: {
-    cost: 20,
-    p3: 24+22,
-    p4: 15+17
-  },
-  s12: {
-    cost: 20,
-    p1: 12+15,
-    p2: 11+11,
-    p3: 10+9,
-    p4: 15+17
-  }
-};
+console.log(`daysLeft: ${daysLeft}`);
+start({daysLeft});
 
-const integers = [
-  ...Object.keys(variables)
-];
+// console.log(`daysLeft: ${daysLeft + 1}`);
+// start({daysLeft: daysLeft + 1});
 
-const result = solve({
-  direction: "minimize",
-  objective: "cost",
-  constraints,
-  variables,
-  integers,
-});
 
-console.log(result);
+function start({daysLeft}) {
+  const constraints = {
+    p1: greaterEq(
+      Math.max(
+        dailyConstraint.p1 * daysLeft || 0,
+        shop.p1
+        - current.p1
+        - (dailyGain.p1 || 0) * dailyMultiplier * daysLeft
+      )
+    ),
+    p2: greaterEq(
+      Math.max(
+        dailyConstraint.p2 * daysLeft || 0,
+        shop.p2
+        - current.p2
+        - (dailyGain.p2 || 0) * dailyMultiplier * daysLeft
+      )
+    ),
+    p3: greaterEq(
+      Math.max(
+        dailyConstraint.p3 * daysLeft || 0,
+        shop.p3
+        - current.p3
+        - (dailyGain.p3 || 0) * dailyMultiplier * daysLeft
+      )
+    ),
+    // p4: greaterEq(
+    //   Math.max(
+    //     dailyConstraint.p4 * daysLeft || 0,
+    //     shop.p4
+    //     - current.p4
+    //     - (dailyGain.p4 || 0) * dailyMultiplier * daysLeft
+    //   )
+    // )
+  };
+  console.log(constraints)
+
+  const integers = [
+    ...Object.keys(stages)
+  ];
+
+  const result = solve({
+    direction: "minimize",
+    objective: "cost",
+    constraints,
+    variables: stages,
+    integers,
+  });
+
+  console.log(result);
+
+}

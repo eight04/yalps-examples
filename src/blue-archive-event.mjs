39e7f13aab7f22108a819c9dfda0e8b47653daff
eight04 <eight04@gmail.com>
2023-09-27 00:58:23 +0800

    Merge ba box into event

--- src/blue-archive-event.mjs
+++ src/blue-archive-event.mjs
@@ -1,7 +1,16 @@
 import {solve, greaterEq, lessEq} from "yalps";
 
+const shopApMultiplier = 4;
 const dailyGain = {
   // p4: 40+40+40+50+50+40+50+50
+  cost: (
+    24*60/6+
+    150+
+    200/7+
+    25.15*24 +
+    90*shopApMultiplier +
+    - 20*3 - 15*6
+  )
 };
 
 // FIXME: we sum dailyConstraint into dailyConstraint * daysLeft
@@ -13,63 +22,68 @@ const dailyConstraint = {
 };
 
 const dailyMultiplier = 1;
-const endDate = new Date("2023-09-19T09:59");
+const endDate = new Date("2023-10-10T09:59");
 const daysLeft = Math.floor((endDate - Date.now()) / (1000 * 60 * 60 * 24));
 
 const current = {
-  p1: 3238,
-  p2: 5770,
-  p3: 10750,
-  p4: 14577
+  p1: 1587,
+  p2: 1583,
+  p3: 0,
+  p4: 2456
 }
 
 const shop = {
-  p1: 10*40+30*15+20*30+10*60,
+  p1: (
+    3*10+18*30+12*100+4*300+
+    45*5+22*15+12*50+6*200+
+    45*5+22*15+12*50+6*200+
+    1000+2000+2000
+  ),
   p2: (
-    35*20+30*40+25*100+7*200
+    50*5+38*15+25*50+15*200+
+    45*5+22*15+12*50+6*200+
+    45*5+22*15+12*50+6*200+
+    500*2+2000
   ),
   p3: (
-    5*150+60*15+30*50+15*150 +
-    5*150+60*15+30*50+15*150
+    0
   ),
   p4: (
-    15000
+    0
   )
 }
 
 const stages = {
   s9: {
     cost: 20,
-    p1: 40+48,
-    p4: 8+10
+    p1: 6+8,
+    p2: 6+8,
+    p4: 21+26
   },
   s10: {
     cost: 20,
-    p2: 32+39,
-    p4: 18
+    p1: 21+26,
+    p4: 12+15
   },
   s11: {
     cost: 20,
-    p3: 27+33,
-    p4: 18
+    p2: 21+27,
+    p4: 12+15
   },
   s12: {
     cost: 20,
-    p1: 6+8,
-    p2: 6+8,
-    p3: 6+8,
-    p4: 32+39
+    p4: 30+36
   }
 };
 
 console.log(`daysLeft: ${daysLeft}`);
-start({daysLeft});
+start({daysLeft, goal: {max: "p4"}});
 
 // console.log(`daysLeft: ${daysLeft + 1}`);
 // start({daysLeft: daysLeft + 1});
 
 
-function start({daysLeft}) {
+function start({daysLeft, goal}) {
   const constraints = {
     p1: greaterEq(
       Math.max(
@@ -102,7 +116,10 @@ function start({daysLeft}) {
         - current.p4
         - (dailyGain.p4 || 0) * dailyMultiplier * daysLeft
       )
-    )
+    ),
+    cost: lessEq(
+      dailyGain.cost * dailyMultiplier * daysLeft
+    ),
   };
   console.log(constraints)
 
@@ -111,8 +128,8 @@ function start({daysLeft}) {
   ];
 
   const result = solve({
-    direction: "minimize",
-    objective: "cost",
+    direction: goal.max ? "maximize" : "minimize",
+    objective: goal.max || goal.min || "cost",
     constraints,
     variables: stages,
     integers,

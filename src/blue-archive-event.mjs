50ad656fcf3927e660bb92a0651daf241b4a1db4
eight04 <eight04@gmail.com>
2024-06-26 01:26:48 +0800

    Camp event

--- src/blue-archive-event.mjs
+++ src/blue-archive-event.mjs
@@ -19,22 +19,22 @@ const dailyGain = {
 };
 
 const dailyMultiplier = 1;
-const endDate = new Date("2024-05-14T09:59+08:00");
+const endDate = new Date("2024-07-09T09:59+08:00");
 const daysLeft = Math.floor((endDate - Date.now()) / (1000 * 60 * 60 * 24));
 
 // FIXME: we sum dailyConstraint into dailyConstraint * daysLeft
 // which is wrong, the constraint has to be met every day
 const dailyConstraint = {
-  p1: 0 / daysLeft,
+  p1: 900 / daysLeft,
   p2: 0 / daysLeft,
   p3: 0 / daysLeft,
   p4: 0 / daysLeft,
 };
 
 const current = {
-  p1: 3195,
-  p2: 2493,
-  p3: 2493,
+  p1: 2660,
+  p2: 2807,
+  p3: 2825,
   p4: 0
 };
 
@@ -42,16 +42,16 @@ const shop = {
   p1: (0
   ),
   p2: ( 0
-    +150+75*5+35*20+15*100
-    +10*10+5*30+3*100+300
-    +10*10+5*30+3*100+300
-    +10*10+5*30+3*100+300
+    +18*30+12*100+4*300
+    +12*50+6*200
+    +45*5+22*15+12*50+6*200
     +300+2000
   ),
   p3: ( 0
-    +12*15+8*50+5*200
-    +12*15+8*50+5*200
-    +12*15+8*50+5*200
+    +180+95*4+30*15+12*60
+    +38*15+25*50+15*200
+    +12*50+6*200
+    +12*50+6*200
     +300+2000
   ),
   p4: ( 0
@@ -59,78 +59,78 @@ const shop = {
 }
 
 const bonus = {
-  p1: 50+30+30,
-  p2: 25+45+30,
-  p3: 30+20+25+15,
+  p1: 15+15+10+15+15,
+  p2: 15*3+10+15*2,
+  p3: 15*2+20+15*2,
   p4: 0,
 };
 
 console.log("bonus:", bonus);
 
 const stages = {
-  // t3: {
-  //   p2: -5,
-  //   p3: 1
-  // },
-  // t4: {
-  //   p3: -5,
-  //   p4: 1
-  // },
-  // s1: {
-  //   cost: 10,
-  //   ...p(3, 9, 3, 3)
-  // },
-  // s2: {
-  //   cost: 10,
-  //   ...p(3, 3, 9, 3)
-  // },
-  // s3: {
-  //   cost: 10,
-  //   ...p(3, 3, 3, 9)
-  // },
-  // s4: {
-  //   cost: 10,
-  //   ...p(18, 0, 0, 0)
-  // },
-  // s5: {
-  //   cost: 15,
-  //   ...p(4, 18, 5, 0)
-  // },
-  // s6: {
-  //   cost: 15,
-  //   ...p(4, 0, 18, 5)
-  // },
-  // s7: {
-  //   cost: 15,
-  //   ...p(4, 5, 0, 18)
-  // },
-  // s8: {
-  //   cost: 15,
-  //   ...p(27, 0, 0, 0)
-  // },
+  t3: {
+    p2: -5,
+    p3: 1
+  },
+  t4: {
+    p3: -5,
+    p4: 1
+  },
+  s1: {
+    cost: 10,
+    ...p(6,6,6,0)
+  },
+  s2: {
+    cost: 10,
+    ...p(6,9,3,0)
+  },
+  s3: {
+    cost: 10,
+    ...p(6,3,9,0)
+  },
+  s4: {
+    cost: 10,
+    ...p(8,4,4,0)
+  },
+  s5: {
+    cost: 15,
+    ...p(5,11,11,0)
+  },
+  s6: {
+    cost: 15,
+    ...p(5,17,5,0)
+  },
+  s7: {
+    cost: 15,
+    ...p(5,5,17,0)
+  },
+  s8: {
+    cost: 15,
+    ...p(16,4,4,0)
+  },
   s9: {
     cost: 20,
-    ...p(28, 4, 4)
+    ...p(4,16,16,0)
   },
   s10: {
     cost: 20,
-    ...p(4, 32, 0)
+    ...p(4,32,0,0)
   },
   s11: {
     cost: 20,
-    ...p(4, 0, 32)
+    ...p(4,0,32,0)
   },
   s12: {
     cost: 20,
-    ...p(36, 0, 0)
+    ...p(32,0,0,0)
   },
 };
 
 applyBonus(stages, bonus);
 
-// for (const key in stages) {
-//   stages[key].length = sum([1, 2, 3, 4].map(i => stages[key][`p${i}`] || 0)) / stages[key].cost;
-// }
+for (const key in stages) {
+  stages[key].length = sum([1, 2, 3, 4].map(i => stages[key][`p${i}`] || 0)) / stages[key].cost;
+}
 
 function sum(arr) {
   return arr.reduce((a, b) => a + b, 0);

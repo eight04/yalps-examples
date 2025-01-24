19421b249120dc3afddab9e11061907effc4b3f1
eight04 <eight04@gmail.com>
2025-01-24 23:27:33 +0800

    sheside outside

--- src/blue-archive-event.mjs
+++ src/blue-archive-event.mjs
@@ -1,16 +1,18 @@
 import {solve, greaterEq, lessEq} from "yalps";
 
-const apFromShop = 90 * 0;
+const apFromShop = 90 * 3;
 const apFromDiamonds = 120 * 0;
-const apToSchoolExchange = 15 * 3;
+const apToSchoolExchange = 15 * 6;
 const apToHard = 20 * 3 * 1;
+const apNaturalGeneration = 24 * 60 / 6;
+const apCofeGeneration = 670;
 const dailyGain = {
   // p4: 40+40+40+50+50+40+50+50
   cost: Math.floor(
-    24*60/6+
-    150+
-    200/7+
-    25.15*24 +
+    apNaturalGeneration +
+    150+ // daily mission
+    200/7+ // weekly mission
+    apCofeGeneration +
     apFromDiamonds +
     apFromShop +
     - apToHard // hard main
@@ -19,7 +21,7 @@ const dailyGain = {
 };
 
 const dailyMultiplier = 1;
-const endDate = new Date("2024-11-19T09:59+08:00");
+const endDate = new Date("2025-02-08T09:59+08:00");
 const daysLeft = Math.floor((endDate - Date.now()) / (1000 * 60 * 60 * 24));
 
 // FIXME: we sum dailyConstraint into dailyConstraint * daysLeft
@@ -32,9 +34,9 @@ const dailyConstraint = {
 };
 
 const current = {
-  p1: 2172,
-  p2: 1488,
-  p3: 1504,
+  p1: 1137,
+  p2: 1423,
+  p3: 1404,
   p4: 0
 };
 
@@ -42,21 +44,22 @@ const shop = {
   p1: (0
   ),
   p2: ( 0
+    +25*10+15*30+10*100+3*300
     +3*300
-    +70*5+10*200
-    +1000+300
+    +2000+300+300
   ),
   p3: ( 0
-    +70*5+10*200
-    +1000+300
+    +30*15+20*50
+    +10*200
+    +2000+300+300
   ),
-  p4: 4*5+20*2+20
+  p4: 0
 }
 
 const bonus = {
   p1: 25+25+15+15+15+15,
-  p2: 20+20+15+15+20+15,
-  p3: 15+15+15+15+20+15,
+  p2: 30+15+15+15+20+15,
+  p3: 30+15+15+15+20+15,
   p4: 0,
 };
 
@@ -73,60 +76,56 @@ const stages = {
   // },
   s1: {
     cost: 10,
-    ...p(12,3,3)
+    ...p(4,7,7)
   },
   s2: {
     cost: 10,
-    ...p(0,14,4)
+    ...p(0,12,6)
   },
   s3: {
     cost: 10,
-    ...p(0,4,14)
+    ...p(0,6,12)
   },
   s4: {
     cost: 10,
-    ...p(18,0,0)
+    ...p(12,3,3)
   },
   s5: {
     cost: 15,
-    ...p(19,4,4)
+    ...p(3,12,12)
   },
   s6: {
     cost: 15,
-    ...p(0,23,4)
+    ...p(0,21,6)
   },
   s7: {
     cost: 15,
-    ...p(0,4,23)
+    ...p(0,6,21)
   },
   s8: {
     cost: 15,
-    ...p(27,0,0)
+    ...p(21,3,3)
   },
   s9: {
     cost: 20,
-    ...p(28,4,4)
+    ...p(0,18,18)
   },
   s10: {
     cost: 20,
-    ...p(4,32,0)
+    ...p(0,36,0)
   },
   s11: {
     cost: 20,
-    ...p(4,0,32)
+    ...p(0,0,36)
   },
   s12: {
     cost: 20,
-    ...p(36,0,0)
+    ...p(32,2,2)
   },
   sHard: {
     cost: 60,
     hard: 1
   },
-  p1toP4: {
-    p1: 184-3784,
-    p4: 36
-  }
 };
 
 applyBonus(stages, bonus);
@@ -141,8 +140,8 @@ function sum(arr) {
 }
 
 console.log(`daysLeft: ${daysLeft}`);
-// start({daysLeft, goal: {max: "p1"}});
-start({daysLeft, goal: {max: "hard"}});
+start({daysLeft, goal: {max: "p1"}});
+// start({daysLeft, goal: {max: "hard"}});
 
 // console.log(`daysLeft: ${daysLeft + 1}`);
 // start({daysLeft: daysLeft + 1});
